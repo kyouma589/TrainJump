bfbbbc0b25a9d71148537084424b3d4ab92cd986
kyouma589 <rlothian589@gmail.com>
2020-09-18 13:44:06 -0400

    Train now jumps!

--- TrainJump/Assets/Scripts/TrainMove.cs
+++ TrainJump/Assets/Scripts/TrainMove.cs
@@ -6,19 +6,24 @@ public class TrainMove : MonoBehaviour
 {
 
     public float moveSpeed = 2f;
-    public Sprite train;
+    public float jumpHeight = 2f;
+    public Rigidbody2D player;
 
     // Start is called before the first frame update
     void Start()
     {
         
+        player = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        
+        if (Input.GetKeyDown("space") && Mathf.Abs(player.velocity.y) < 0.001f)
+        {
+            player.AddForce(new Vector2(0, jumpHeight),ForceMode2D.Impulse);
+        }
 
     }
 }
