e495b45d697746544c6eafc6b33318207f647767
kyouma589 <rlothian589@gmail.com>
2020-09-18 12:53:29 -0400

    added movement script

---
+++ TrainJump/Assets/Scripts/TrainMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainMove : MonoBehaviour
+{
+
+    public float moveSpeed = 2f;
+    public Sprite train;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+    }
+}
