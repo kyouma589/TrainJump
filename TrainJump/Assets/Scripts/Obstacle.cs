968282fc6a05ff7c5261db85da8a99815df5608f
kyouma589 <rlothian589@gmail.com>
2020-09-22 12:29:15 -0400

    Added start and restart functions, added obstacle

---
+++ TrainJump/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+
+    public int damage = 1;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<TrainMove>().health -= damage;
+            Destroy(gameObject);
+        }
+
+
+    }
+}
--- TrainJump/Assets/Scripts/TrainMove.cs
+++ TrainJump/Assets/Scripts/TrainMove.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TrainMove : MonoBehaviour
 {
+    public bool gameStart = false;
+
+    public Text startText;
 
     public float moveSpeed = 2f;
     public float jumpHeight = 2f;
     public Rigidbody2D player;
+
+    public int health = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +26,30 @@ public class TrainMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown("space") && Mathf.Abs(player.velocity.y) < 0.001f)
+        if (gameStart == false)
         {
-            player.AddForce(new Vector2(0, jumpHeight),ForceMode2D.Impulse);
+
+            if (Input.GetKeyDown("space")) {
+                startText.text = "";
+                gameStart = true;
+            }
+            
         }
+        else
+        {
+            if (health <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+
+            if (Input.GetKeyDown("space") && Mathf.Abs(player.velocity.y) < 0.001f)
+            {
+                player.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+            }
+        }
+       
+
+       
 
     }
 }
